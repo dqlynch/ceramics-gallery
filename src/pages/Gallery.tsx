87fd40c49179ffd437ba7e9699651456bef2c7ad
Dylan Lynch <dylan.lynch@kraken.tech>
2025-05-27 12:14:04 -0700

    Add gallery images

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -47,7 +47,16 @@ const Gallery = () => {
           '/images/gallery/5.jpg',
           '/images/gallery/6.jpg',
           '/images/gallery/7.jpg',
-          '/images/gallery/8.jpg'
+          '/images/gallery/8.jpg',
+          '/images/gallery/9.jpg',
+          '/images/gallery/10.jpg',
+          '/images/gallery/11.jpg',
+          '/images/gallery/12.jpg',
+          '/images/gallery/13.jpg',
+          '/images/gallery/14.jpg',
+          '/images/gallery/15.jpg',
+          '/images/gallery/16.jpg',
+          '/images/gallery/17.jpg',
         ];
 
         const imagePromises = imageFiles.map(async (src) => {
