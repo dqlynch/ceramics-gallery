1972a7a713d6ef3493793caa60ad93fb33fbef73
Dylan Lynch <dylan.lynch@kraken.tech>
2025-05-28 09:04:42 -0700

    Update social icons in navbar: - Add Venmo icon using PNG - Make both icons use theme accent color on hover - Fix styling issues with Emotion

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
+import venmoLogo from '../assets/venmo_v.png';
 
 const Nav = styled.nav`
   background-color: ${props => props.theme.colors.background};
@@ -38,7 +39,21 @@ const NavLink = styled(Link)`
   }
 `;
 
-const InstagramLink = styled.a`
+const SocialIcon = styled.img`
+  width: 20px;
+  height: 20px;
+  filter: brightness(0);
+  transition: filter 0.2s ease;
+`;
+
+const SocialSvg = styled.svg`
+  width: 20px;
+  height: 20px;
+  fill: currentColor;
+  transition: fill 0.2s ease;
+`;
+
+const SocialLink = styled.a`
   color: ${props => props.theme.colors.text};
   text-decoration: none;
   display: flex;
@@ -47,12 +62,6 @@ const InstagramLink = styled.a`
   
   &:hover {
     color: ${props => props.theme.colors.accent};
-  }
-
-  svg {
-    width: 20px;
-    height: 20px;
-    fill: currentColor;
   }
 `;
 
@@ -64,16 +73,24 @@ const Navbar = () => {
         <NavLinks>
           <NavLink to="/">Shop</NavLink>
           <NavLink to="/gallery">Gallery</NavLink>
-          <InstagramLink 
+          <SocialLink 
             href="https://www.instagram.com/d.q.lynch/" 
             target="_blank" 
             rel="noopener noreferrer"
             aria-label="Instagram"
           >
-            <svg viewBox="0 0 24 24">
+            <SocialSvg viewBox="0 0 24 24">
               <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
-            </svg>
-          </InstagramLink>
+            </SocialSvg>
+          </SocialLink>
+          <SocialLink 
+            href="https://venmo.com/Dylan-Lynch-2" 
+            target="_blank" 
+            rel="noopener noreferrer"
+            aria-label="Venmo"
+          >
+            <SocialIcon src={venmoLogo} alt="Venmo" />
+          </SocialLink>
         </NavLinks>
       </NavContainer>
     </Nav>
